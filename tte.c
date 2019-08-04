83ae37f0650911032cd5b309774f1fb57a3d4046
acouprie <acouprie@hotmail.fr>
2019-08-04 19:17:10 +0200

    raw mode

--- tte.c
+++ tte.c
@@ -1,7 +1,66 @@
+/*** includes ***/
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
 
+/*** data ***/
+
+struct termios orig_termios;
+
+/*** terminal ***/
+
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
+void disableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
+}
+
+/*
+  In default canonical mode,
+  inputs are only sent when Enter is pressed.
+  Need to switch to Raw mode to disable this behavior
+*/
+void enableRawMode() {
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+  // diable Raw mode at exit
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+  tcgetattr(STDIN_FILENO, &raw);
+
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);;
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+/*** init ***/
+
 int main() {
-	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
-	return 0;
+  enableRawMode();
+
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    // leave when q + Enter is pressed
+    if (c == 'q') break;
+  }
+  return 0;
 }
